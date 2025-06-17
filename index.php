44a9762cd304d1cdaa3bd00e72dd2c502324adbe
Matt Cabarrubias <monapwc2020@gmail.com>
2025-06-17 14:46:23 +0800

    feat: moved routes to routes/web.php

--- index.php
+++ index.php
@@ -3,18 +3,6 @@ session_start();
 
 require_once 'config/DB_Connect.php';
 require_once 'config/Base_Url.php';
+require_once 'routes/web.php';
 
-require_once 'app/core/Router.php';
-
-$router = new Router();
-$router->get('', 'AuthController@showLogin');
-$router->get('login', 'AuthController@showLogin');
-$router->post('login', 'AuthController@login');
-$router->get('signup', 'AuthController@showSignup');
-$router->post('signup', 'AuthController@signup');
-$router->get('home', 'AuthController@home');
-$router->get('logout', 'AuthController@logout');
-
-$url = $_GET['url'] ?? '';
-$router->handleRequest($url);
 ?>
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'app/core/Router.php';
+
+$router = new Router();
+$router->get('', 'AuthController@showLogin');
+$router->get('login', 'AuthController@showLogin');
+$router->post('login', 'AuthController@login');
+$router->get('signup', 'AuthController@showSignup');
+$router->post('signup', 'AuthController@signup');
+$router->get('home', 'AuthController@home');
+$router->get('logout', 'AuthController@logout');
+
+$url = $_GET['url'] ?? '';
+$router->handleRequest($url);
+?>
