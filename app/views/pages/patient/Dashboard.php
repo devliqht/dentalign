953aba39ab9e1afebcee76202cf10b1597fca57a
Matt Cabarrubias <monapwc2020@gmail.com>
2025-06-19 14:36:23 +0800

    feat: removed pages/Home and created pages/patient/Dashboard specific

---
+++ app/views/pages/patient/Dashboard.php
@@ -0,0 +1,11 @@
+<div class="max-w-4xl px-4 py-8">
+    <div class="p-6">
+        <h2 class="text-2xl font-bold text-gray-900 mb-4"><?php echo $_SESSION['user_type'] . ' Dashboard'; ?></h2>
+        <p class="text-gray-600 mb-4">You have successfully logged into your account. This is your dashboard where you can manage your dental care activities.</p>
+    </div>
+    <div class="flex flex-row gap-4 p-6">
+        <div class="flex flex-col">
+            <h1 class="text-2xl font-bold font-family-sans text-nhd-blue">Upcoming Appoinments</h1>
+        </div>
+    </div>
+</div> 
