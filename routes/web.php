1c2b1397a154cbadae9a8ca73caa3d7dd71f0041
Matt Cabarrubias <monapwc2020@gmail.com>
2025-06-19 14:36:34 +0800

    feat: add Patient and Doctor routes to Router

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,38 @@ $router->group("GET", "AuthController", [
     "" => "DisplayLoginPage",
     "login" => "DisplayLoginPage",
     "signup" => "DisplaySignupPage",
-    "home" => "DisplayHomePage",
+    "dashboard" => "DisplayHomePage",
     "logout" => "LogoutUser",
 ]);
 
 $router->group("POST", "AuthController", [
     "login" => "LoginUser",
     "signup" => "SignupUser",
+]);
+
+$router->group("GET", "PatientController", [
+    "patient/dashboard" => "dashboard",
+    "patient/bookings" => "bookings",
+    "patient/book-appointment" => "bookAppointment",
+    "patient/payments" => "payments",
+    "patient/results" => "results",
+]);
+
+$router->group("POST", "PatientController", [
+    "patient/book-appointment" => "storeAppointment",
+]);
+
+$router->group("GET", "DoctorController", [
+    "doctor/dashboard" => "dashboard",
+    "doctor/schedule" => "schedule",
+    "doctor/appointment-history" => "appointmentHistory",
+    "doctor/patient-records" => "patientRecords",
+    "doctor/inbox" => "inbox",
+]);
+
+$router->group("POST", "DoctorController", [
+    "doctor/schedule" => "updateSchedule",
+    "doctor/patient-records" => "updatePatientRecord",
 ]);
 
 $url = $_GET["url"] ?? "";
