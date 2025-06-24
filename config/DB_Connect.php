778c425ac05da4aa8bdea11f4f54d50abd1881a2
Matt Cabarrubias <monapwc2020@gmail.com>
2025-06-24 15:58:23 +0800

    fix: 2 🙏😭

--- config/DB_Connect.php
+++ config/DB_Connect.php
@@ -9,7 +9,7 @@ define("DB_HOST", "localhost:3306");
 define("DB_USER", "s22103604_dentalign");
 define("DB_PASS", "sonicdravice");
 define("DB_NAME", "s22103604_dentalign");
-define("DB_SOCKET", "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock");
+// define("DB_SOCKET", "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock");
 
 $conn = @new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, 3306, DB_SOCKET);
 
