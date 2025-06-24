5729c1c481441612a9ce15c1a97d2556954e2862
Matt Cabarrubias <monapwc2020@gmail.com>
2025-06-24 16:07:10 +0800

    feat: revert back i guess it didnt work

--- config/DB_Connect.php
+++ config/DB_Connect.php
@@ -1,17 +1,17 @@
 <?php
-// define("DB_HOST", "localhost");
-// define("DB_USER", "root");
-// define("DB_PASS", "");
-// define("DB_NAME", "dentalign");
-// define("DB_SOCKET", "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "");
+define("DB_NAME", "dentalign");
+define("DB_SOCKET", "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock");
 
-define("DB_HOST", "localhost:3306");
-define("DB_USER", "s22103604_dentalign");
-define("DB_PASS", "sonicdravice");
-define("DB_NAME", "s22103604_dentalign");
-// define("DB_SOCKET", "/Applications/XAMPP/xamppfiles/var/mysql/mysql.sock");
+// define("DB_HOST", "localhost:3306");
+// define("DB_USER", "s22103604_dentalign");
+// define("DB_PASS", "sonicdravice");
+// define("DB_NAME", "s22103604_dentalign");
 
-$conn = @new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, 3306);
+
+$conn = @new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, 3306, DB_SOCKET);
 
 if ($conn->connect_error) {
     $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, 3306);
